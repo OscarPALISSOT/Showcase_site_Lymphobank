44e65733c0e48a8442871a9d4a9d7dcde57927a7
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-05-18 15:11:45 +0200

    commit

---
+++ src/Controller/admin/ActuController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Controller\admin;
+
+use App\Entity\Actu;
+use App\Form\ActuFormType;
+use App\Repository\ActuRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ActuController extends AbstractController
+{
+    public function __construct(ActuRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+
+    /**
+     * @Route ("/Admin/Actus", name="gestion_actus")
+     * @return Response
+     */
+    public function ShowActu(PaginatorInterface $paginator, Request $request): Response
+    {
+
+        $actus = $paginator->paginate(
+            $this->repository->findBy(array(), array('created_at' => 'DESC')),
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        return $this->render('admin/actus/ShowActus.html.twig', [
+            'actus' => $actus,
+            'loggedUser' => $this->getUser(),
+        ]);
+    }
+
+    /**
+     * @Route ("/Admin/NouveauActu", name="create_actu")
+     * @return Response
+     */
+    public function createActu(Request $request){
+
+        $actu = new Actu;
+        $form = $this->createForm(ActuFormType::class, $actu);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($actu);
+            $em->flush();
+            return $this->redirectToRoute('gestion_actus');
+        }
+        return $this->render('admin/actus/CreateActu.html.twig', [
+            'actu' => $actu,
+            'loggedUser' => $this->getUser(),
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    /**
+     * @Route ("/Admin/Actus/{id}", name="edit_actu")
+     * @return Response
+     */
+    public function editActu(Actu $actu, Request $request){
+        $form = $this->createForm(ActuFormType::class, $actu);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+            return $this->redirectToRoute('gestion_actus');
+        }
+        return $this->render('admin/actus/EditActu.html.twig', [
+            'actu' => $actu,
+            'loggedUser' => $this->getUser(),
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route ("/Admin/Actu/{id}", name="delete_actu", methods="DELETE")
+     * @return Response
+     */
+    public function deleteActu(Actu $actu, Request $request){
+
+        if ($this->isCsrfTokenValid("delete", $request->get('_token'))){
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($actu);
+            $em->flush();
+        }
+        
+        return $this->redirectToRoute('gestion_actus');
+    }
+
+}
---
+++ src/Entity/Actu.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ActuRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ActuRepository::class)
+ */
+class Actu
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $sousTitre;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $article;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getSousTitre(): ?string
+    {
+        return $this->sousTitre;
+    }
+
+    public function setSousTitre(?string $sousTitre): self
+    {
+        $this->sousTitre = $sousTitre;
+
+        return $this;
+    }
+
+    public function getArticle(): ?string
+    {
+        return $this->article;
+    }
+
+    public function setArticle(string $article): self
+    {
+        $this->article = $article;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+}
