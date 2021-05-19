a8b3ddfb59b5828afb9283176037828d05846af6
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-05-19 13:49:48 +0200

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
-    .addEntry('base', './assets/base.js')
+    .addStyleEntry('/scss/base', './assets/styles/base.scss')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
