0f3bbd2525147a7a94e405d56e6c831e2a2c0bc2
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-05-19 15:40:34 +0200

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addStyleEntry('/scss/base', './assets/styles/base.scss')
+    .addStyleEntry('/scss/admin', './assets/styles/admin.scss')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
