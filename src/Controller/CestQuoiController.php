6af2bd81e12a7cc99f8884ca90cd4d397b9cc5b7
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-05-28 17:42:49 +0200

    commit

---
+++ src/Controller/CestQuoiController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+class CestQuoiController extends AbstractController
+{
+
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+
+
+    /**
+     * @Route ("/Qu-est-ce-que-le-don", name="cestquoi")
+     * @return Response
+     */
+    public function index() : Response{
+
+        return new Response(content: $this->twig->render('pages/cestquoi.html.twig', [
+            
+        ]));
+    }
+}
+?>
