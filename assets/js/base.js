49ccd3f1a5438551dff849d2b5bc1e06cda70a72
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-05-31 16:23:18 +0200

    commit

--- assets/js/base.js
+++ assets/js/base.js
@@ -33,3 +33,25 @@ var acc = document.getElementsByClassName("accordion");
         }
     });
     // fin btn menu burger
+
+    // bouton card
+
+    var btnCard = document.getElementsByClassName("btnCard");
+    
+    var i;
+
+    for (i = 0; i < btnCard.length; i++) {
+    btnCard[i].addEventListener("click", function() {
+        this.classList.toggle("activeCard");
+        var panelCard = this.nextElementSibling;
+        var card = this.parentElement;
+        if (panelCard.style.maxHeight) {
+        panelCard.style.maxHeight = null;
+        card.style.maxHeight = null;
+        } else {
+        panelCard.style.maxHeight = panelCard.scrollHeight + "px";
+        card.style.maxHeight = card.scrollHeight + panelCard.scrollHeight + "px";
+        }
+    });
+    }
+    // fin bouton card
