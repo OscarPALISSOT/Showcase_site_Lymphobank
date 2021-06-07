3aa7a6200555068b242f1209aaede8737e2eb0a5
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-06-07 11:59:20 +0200

    commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,20 +4,23 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\TemoignageRepository;
 use Twig\Environment;
 
 class HomeController extends AbstractController {
 
 
-    public function __construct(Environment $twig)
+    public function __construct(Environment $twig, TemoignageRepository $repository)
     {
         $this->twig = $twig;
+        $this->repository = $repository;
     }
 
     public function index() : Response{
 
+        $temoignages = $this->repository->findAll();
         return new Response(content: $this->twig->render('pages/home.html.twig', [
-            
+            'temoignages' => $temoignages,
         ]));
     }
 
