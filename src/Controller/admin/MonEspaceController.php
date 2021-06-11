db82da8efffbaa4dbaee8df2f3f13611cc392919
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-06-11 21:52:19 +0200

    commit

--- src/Controller/admin/MonEspaceController.php
+++ src/Controller/admin/MonEspaceController.php
@@ -5,40 +5,77 @@ namespace App\Controller\admin;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Admins;
+use App\Form\AdminsFormType;
+use App\Form\ChangePwdType;
 use Twig\Environment;
+use Symfony\Component\Form\FormError;
 
 
 class MonEspaceController extends AbstractController {
 
 
-    public function __construct(Environment $twig)
+    public function __construct(Environment $twig, UserPasswordEncoderInterface $encoder)
     {
         $this->twig = $twig;
+        $this->encoder = $encoder;
     }
 
     /**
      * @Route ("Admin/MonEspace/{id}", name="monEspace")
      * @return Response
      */
-    public function index(Request $request) :Response {
+    public function index(Admins $admin, Request $request) :Response {
 
-        $admin = $this->getUser();
+        $user = $this->getUser();
+        $em = $this->getDoctrine()->getManager();
+        $form = $this->createForm(AdminsFormType::class, $admin);
+        $form->handleRequest($request);
+        $formPwd = $this->createForm(ChangePwdType::class, $user);
+    	$formPwd->handleRequest($request);
 
-        
-        
-        if ($this->isCsrfTokenValid("editAdmin", $request->get('_token'))){
-            $login = $request->request->get('username');
-            $pwd = $request->request->get('password');
-            $admin->setUsername($login);
-            $admin->setPassword($this->encoder->encodePassword($admin, $pwd));
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($admin);
+        if ($form->isSubmitted() && $form->isValid()){
             $em->flush();
+            return $this->redirectToRoute('monEspace', [
+                'id' => $this->getUser()->getUsername()
+            ]);
         }
+
+        if ($formPwd->isSubmitted() && $formPwd->isValid()) {
+
+            $oldPassword = $formPwd["oldPassword"]->getData();
+
+            // Si l'ancien mot de passe est bon
+
+            if ($this->encoder->isPasswordValid($user, $oldPassword)) {
+
+                $newEncodedPassword = $this->encoder->encodePassword($user, $formPwd["plainPassword"]->getData());
+
+                $user->setPassword($newEncodedPassword);
+
+                $em->persist($user);
+
+                $em->flush();
+
+                $this->addFlash('notice', 'Votre mot de passe à bien été changé !');
+
+                return $this->redirectToRoute('monEspace', [
+                    'id' => $this->getUser()->getUsername()
+                ]);
+            } else {
+
+                $formPwd->addError(new FormError('Ancien mot de passe incorrect'));
+
+            }
+        }
+        
         return new Response(content:$this->twig->render('admin/monEspace/monEspace.html.twig', [
             'loggedUser' => $this->getUser(),
+            'form' => $form->createView(),
+            'formPwd' => $formPwd->createView(),
         ]));
 
     }
