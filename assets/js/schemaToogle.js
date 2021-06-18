44716f4b52a8f018dc71d7f174081e5c327bb624
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-06-18 16:57:00 +0200

    commit

--- assets/js/schemaToogle.js
+++ assets/js/schemaToogle.js
@@ -3,7 +3,8 @@ var Toogle = document.getElementsByClassName("schemaToogle");
 var schema = document.getElementsByClassName("schemaDonSteep1");
 
 Toogle.addEventListener("click", function() {
-    schema.classList.toggle("active");
+    alert('test');
+    schema.classList.add("active");
     if (schema.style.maxHeight) {
     schema.style.maxHeight = null;
     } else {
