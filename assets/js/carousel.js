aa4508fe12cae6270a63f5a5bd761036e32bca78
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-06-28 16:29:50 +0200

    commit

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,6 +1,7 @@
 
 class Carousel {
 
+    //construit un carousel
     constructor (element, options ={}){
         this.element = element
         this.options = Object.assign({}, {
@@ -9,10 +10,11 @@ class Carousel {
         }, options)
     
         let children = [].slice.call(element.children)
-        let root = this.createDivWithClass('carousel')
+        this.currentItem = 0
+        this.root = this.createDivWithClass('carousel')
         this.container = this.createDivWithClass('carousel__container')
-        root.appendChild(this.container)
-        this.element.appendChild(root)
+        this.root.appendChild(this.container)
+        this.element.appendChild(this.root)
         this.items = children.map((child) => {
             let item = this.createDivWithClass('carousel__item')
             item.appendChild(child)
@@ -20,24 +22,51 @@ class Carousel {
             return item
         })
         this.setStyle()
+        this.createNavigation()
     }
 
+    //définit la width des item et du carousel selon les options
     setStyle () {
         let ratio = this.items.length / this.options.slideVisibles
         this.container.style.width = (ratio * 100) + "%"
         this.items.forEach(item => item.style.width = ((100 / this.options.slideVisibles) / ratio) + "%")
     }
     
+    //crée une div avec une classe
     createDivWithClass(className) {
         let div = document.createElement('div')
         div.setAttribute('class', className)
         return div
     }
+
+    //crée les fleches et la nav
+    createNavigation() {
+        let nextButton = this.createDivWithClass('carousel__next')
+        let prevButton = this.createDivWithClass('carousel__prev')
+        this.root.appendChild(nextButton)
+        this.root.appendChild(prevButton)
+        nextButton.addEventListener('click', this.next.bind(this))
+        prevButton.addEventListener('click', this.prev.bind(this))
+    }
+
+    next () {
+        this.gotoItem(this.currentItem + this.options.slideToScroll)
+    }
+
+    prev () {
+        this.gotoItem(this.currentItem - this.options.slideToScroll)
+    }
+
+    //déplace le carousel sur un item ciblé
+    gotoItem (index) {
+        let translateX = index * -100 / this.items.length
+        this.container.style.transform = 'translate3d(' + translateX + '%, 0, 0)'
+        this.currentItem = index
+    }
 }
 
 
-
-
+//carousel témoignage
 new Carousel(document.querySelector('#temoignage'),{
     slideToScroll: 1,
     slideVisibles: 1
