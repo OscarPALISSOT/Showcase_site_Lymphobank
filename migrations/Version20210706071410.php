80d6d5fbd1615d8e40bfa0403670bfdcf712bda2
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-07-06 09:53:32 +0200

    changement police titre

--- migrations/Version20210706071410.php
+++ migrations/Version20210706071410.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210705173820 extends AbstractMigration
+final class Version20210706071410 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20210705173820 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE actu ADD filename VARCHAR(255) DEFAULT NULL, ADD updated_at DATETIME NOT NULL');
         $this->addSql('ALTER TABLE etablissement ADD num VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE actu DROP filename, DROP updated_at');
         $this->addSql('ALTER TABLE etablissement DROP num');
     }
 }
