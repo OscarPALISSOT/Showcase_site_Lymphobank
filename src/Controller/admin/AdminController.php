a76248adcba67722c153f10b09e85cb24c8107e1
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-07-25 20:39:02 +0200

    debut tableau de bord

--- src/Controller/admin/AdminController.php
+++ src/Controller/admin/AdminController.php
@@ -6,14 +6,18 @@ namespace App\Controller\admin;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\EtablissementRepository;
+use App\Repository\TemoignageRepository;
 use Twig\Environment;
 
 
 class AdminController extends AbstractController {
 
 
-    public function __construct(Environment $twig)
+    public function __construct(Environment $twig, EtablissementRepository $EtablissementRepository, TemoignageRepository $temoignageRepository)
     {
+        $this->TemoignageRepository = $temoignageRepository;
+        $this->EtablissementRepository = $EtablissementRepository;
         $this->twig = $twig;
     }
 
@@ -23,7 +27,9 @@ class AdminController extends AbstractController {
      */
     public function index() :Response {
 
-        return $this->render('admin/admin.html.twig', [
+        return $this->render('admin/tableauDeBord.html.twig', [
+            'nbEtablissement' => count($this->EtablissementRepository->findAll()),
+            'nbTemoignage' => count($this->TemoignageRepository->findAll()),
             'loggedUser' => $this->getUser()
         ]);
 
