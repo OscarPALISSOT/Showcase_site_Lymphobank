423ff4a5625bf685aaf5201497e9262ce7438e7a
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-08-20 16:24:30 +0200

    rezise btn faq

--- assets/js/btnFaq.js
+++ assets/js/btnFaq.js
@@ -7,8 +7,18 @@ var i;
 
     function rezize () {
         for (i = 0; i < acc.length; i++) {
-            if (title[i].offsetHeight > 50) {
-                acc[i].style.minHeight = 70 + 'px'
+            const size = title[i].offsetHeight;
+            if (size > 50 && size < 75){
+                acc[i].style.minHeight = 75 + 'px'
+            }
+            if (size > 75 && size < 110){
+                acc[i].style.minHeight = 110 + 'px'
+            }
+            if (size > 110 && size < 145){
+                acc[i].style.minHeight = 145 + 'px'
+            }
+            if (size > 145 && size < 170){
+                acc[i].style.minHeight = 170 + 'px'
             }
         }
     };
