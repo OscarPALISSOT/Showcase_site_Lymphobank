4d07f678f996b39cc9b8d8ddbd83d6111d6181d2
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-08-22 19:46:39 +0200

    btn faq v2

--- assets/js/btnFaq.js
+++ assets/js/btnFaq.js
@@ -3,22 +3,15 @@ var title = document.getElementsByClassName("accordionTitle");
 var i;
 
 
-    window.onload = rezize;
+window.onload = resize;
 
-    function rezize () {
-        for (i = 0; i < acc.length; i++) {
-            const size = title[i].offsetHeight;
-            if (size > 50 && size < 75){
-                acc[i].style.minHeight = 75 + 'px'
-            }
-            if (size > 75 && size < 110){
-                acc[i].style.minHeight = 110 + 'px'
-            }
-            if (size > 110 && size < 145){
-                acc[i].style.minHeight = 145 + 'px'
-            }
-            if (size > 145 && size < 170){
-                acc[i].style.minHeight = 170 + 'px'
-            }
+function resize () {
+    for (i = 0; i < acc.length; i++) {
+        const size = title[i].offsetHeight;
+        if (size > 50 ){
+            const bottom = size - 50
+            const resize = Math.ceil((bottom)/30)
+            acc[i].style.minHeight = 50 + (resize * 30) + 'px'
         }
-    };
+    }
+};
