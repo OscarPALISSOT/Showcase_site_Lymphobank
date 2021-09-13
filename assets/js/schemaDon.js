97f64625728a2a3d59677d5fe0a83f7878a29eab
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-09-13 15:00:45 +0200

    schema don debut js sangcordon

--- assets/js/schemaDon.js
+++ assets/js/schemaDon.js
@@ -1,7 +1,6 @@
 
 var root = document.getElementsByClassName("root");
 
-    
 
 for (i = 0; i < root.length; i++) {
     root[i].addEventListener("click", function() {
@@ -11,5 +10,18 @@ for (i = 0; i < root.length; i++) {
         } else {
             firstStep.style.maxHeight = firstStep.scrollHeight + "px";
         } 
+    });
+}
+
+var sangCordonBtn = document.getElementsByClassName("sangCordonBtn");
+for (i = 0; i < sangCordonBtn.length; i++) {
+    sangCordonBtn[i].addEventListener("click", function() {
+        const sangCordonBtn = document.getElementsByClassName("sangNode");
+        debugger
+        /* if (firstStep.style.maxHeight) {
+            firstStep.style.maxHeight = null;
+        } else {
+            firstStep.style.maxHeight = firstStep.scrollHeight + "px";
+        }  */
     });
 }
