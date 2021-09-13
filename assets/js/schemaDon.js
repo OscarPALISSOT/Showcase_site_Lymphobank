8aa81536cfcaf63a66e87869b1ffd565535d3614
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-09-13 16:58:31 +0200

    suite schema don

--- assets/js/schemaDon.js
+++ assets/js/schemaDon.js
@@ -4,9 +4,11 @@ var root = document.getElementsByClassName("root");
 
 for (i = 0; i < root.length; i++) {
     root[i].addEventListener("click", function() {
+        const sangCordonBtn = document.getElementsByClassName("sangNode")[0];
         const firstStep = this.nextElementSibling;
         if (firstStep.style.maxHeight) {
             firstStep.style.maxHeight = null;
+            sangCordonBtn.style.maxHeight = null;
         } else {
             firstStep.style.maxHeight = firstStep.scrollHeight + "px";
         } 
@@ -16,12 +18,11 @@ for (i = 0; i < root.length; i++) {
 var sangCordonBtn = document.getElementsByClassName("sangCordonBtn");
 for (i = 0; i < sangCordonBtn.length; i++) {
     sangCordonBtn[i].addEventListener("click", function() {
-        const sangCordonBtn = document.getElementsByClassName("sangNode");
-        debugger
-        /* if (firstStep.style.maxHeight) {
-            firstStep.style.maxHeight = null;
+        const sangCordonBtn = document.getElementsByClassName("sangNode")[0];
+        if (sangCordonBtn.style.maxHeight) {
+            sangCordonBtn.style.maxHeight = null;
         } else {
-            firstStep.style.maxHeight = firstStep.scrollHeight + "px";
-        }  */
+            sangCordonBtn.style.maxHeight = sangCordonBtn.scrollHeight + "px";
+        } 
     });
 }
