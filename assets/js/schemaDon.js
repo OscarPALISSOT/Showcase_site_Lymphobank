3815578fc607c6eb494f65b464ac9489b08eea1e
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-09-13 22:42:42 +0200

    schema don suite

--- assets/js/schemaDon.js
+++ assets/js/schemaDon.js
@@ -1,28 +1,41 @@
 
-var root = document.getElementsByClassName("root");
+var root = document.getElementsByClassName("root")[0];
 
 
-for (i = 0; i < root.length; i++) {
-    root[i].addEventListener("click", function() {
-        const sangCordonBtn = document.getElementsByClassName("sangNode")[0];
-        const firstStep = this.nextElementSibling;
-        if (firstStep.style.maxHeight) {
-            firstStep.style.maxHeight = null;
-            sangCordonBtn.style.maxHeight = null;
-        } else {
-            firstStep.style.maxHeight = firstStep.scrollHeight + "px";
-        } 
-    });
-}
+root.addEventListener("click", function() {
+    const sangCordonBtn = document.getElementsByClassName("sangNode")[0];
+    const cordonBtn = document.getElementsByClassName("cordonNode")[0];
+    const firstStep = this.nextElementSibling;
+    if (firstStep.style.maxHeight) {
+        firstStep.style.maxHeight = null;
+        sangCordonBtn.style.maxHeight = null;
+        cordonBtn.style.maxHeight = null;
+    } else {
+        firstStep.style.maxHeight = firstStep.scrollHeight + "px";
+    } 
+});
 
-var sangCordonBtn = document.getElementsByClassName("sangCordonBtn");
-for (i = 0; i < sangCordonBtn.length; i++) {
-    sangCordonBtn[i].addEventListener("click", function() {
-        const sangCordonBtn = document.getElementsByClassName("sangNode")[0];
-        if (sangCordonBtn.style.maxHeight) {
-            sangCordonBtn.style.maxHeight = null;
-        } else {
-            sangCordonBtn.style.maxHeight = sangCordonBtn.scrollHeight + "px";
-        } 
-    });
-}
+var sangCordonBtn = document.getElementsByClassName("sangCordonBtn")[0];
+sangCordonBtn.addEventListener("click", function() {
+    const sangCordonBtn = document.getElementsByClassName("sangNode")[0];
+    const cordonBtn = document.getElementsByClassName("cordonNode")[0];
+    if (sangCordonBtn.style.maxHeight) {
+        sangCordonBtn.style.maxHeight = null;
+    } else {
+        sangCordonBtn.style.maxHeight = sangCordonBtn.scrollHeight + "px";
+        cordonBtn.style.maxHeight = null;
+    } 
+});
+
+
+var cordonBtn = document.getElementsByClassName("cordonBtn")[0];
+cordonBtn.addEventListener("click", function() {
+    const cordonBtn = document.getElementsByClassName("cordonNode")[0];
+    const sangCordonBtn = document.getElementsByClassName("sangNode")[0];
+    if (cordonBtn.style.maxHeight) {
+        cordonBtn.style.maxHeight = null;
+    } else {
+        cordonBtn.style.maxHeight = cordonBtn.scrollHeight + "px";
+        sangCordonBtn.style.maxHeight = null;
+    } 
+});
