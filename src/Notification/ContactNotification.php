8bfd3e4aa6fce0af6fa4c2caa1295e4f62d8a476
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-09-23 16:29:06 +0200

    commit

--- src/Notification/ContactNotification.php
+++ src/Notification/ContactNotification.php
@@ -3,24 +3,10 @@
 namespace App\Notification;
 
 use App\Entity\Contact;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 
 class ContactNotification {
-
-
-
-    /* public function sendMail(Contact $contact) {
-        $message = (new \Swift_Message('contact mon-cordon-precieux.fr'))
-            ->setFrom('noreply@mon-cordon-precieux.fr')
-            ->setTo('contact@mon-cordon-precieux.fr')
-            ->setReplyTo($contact->getMail())
-            ->setBody($this->renderer->render('mails/contact.html.twig', [
-                'contact' => $contact,
-            ]), 'text/html');
-        $this->mailer->send($message);
-    } */
 
     public function __construct(MailerInterface $mailer)
     {
@@ -32,13 +18,8 @@ class ContactNotification {
             ->from('noreply@mon-cordon-precieux.fr')
             ->to('contact@mon-cordon-precieux.fr')
             ->cc($contact->getMail())
-            //->bcc('bcc@example.com')
-            //->replyTo('fabien@example.com')
-            //->priority(Email::PRIORITY_HIGH)
             ->subject('Formulaire contact Mon cordon precieux')
-            // path of the Twig template to render
             ->htmlTemplate('emails/contact.html.twig')
-            // pass variables (name => value) to the template
             ->context([
                 'contact' => $contact,
             ]);
